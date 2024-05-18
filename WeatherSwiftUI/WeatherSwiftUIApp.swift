d984c9c6dbd49ab21a6a381278971b24ef6ce323
Youssef Waleed <ywsoliman@gmail.com>
2024-05-18 14:23:04 +0300

    Initial commit

---
+++ WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Youssef Waleed on 18/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

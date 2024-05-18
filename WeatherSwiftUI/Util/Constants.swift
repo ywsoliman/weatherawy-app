07f5ce2e6beb53c92820a3e4444f66427429496d
Youssef Waleed <ywsoliman@gmail.com>
2024-05-18 16:21:41 +0300

    Create Constants.swift

---
+++ WeatherSwiftUI/Util/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  WeatherSwiftUI
+//
+//  Created by Youssef Waleed on 18/05/2024.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let API_KEY = "4a4ad0d455f14d19bd8115817241805"
+    static let BASE_URL = "http://api.weatherapi.com/v1/forecast.json"
+    
+}

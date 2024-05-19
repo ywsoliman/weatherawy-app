52cd5da1f31166ebc12344d423631ceea8f07afb
Youssef Waleed <ywsoliman@gmail.com>
2024-05-19 18:56:23 +0300

    A function to convert date to time format

--- WeatherSwiftUI/Util/DateUtil.swift
+++ WeatherSwiftUI/Util/DateUtil.swift
@@ -14,3 +14,13 @@ func dayOfWeek(from epoch: TimeInterval) -> String {
     let dayOfWeekString = dateFormatter.string(from: date)
     return dayOfWeekString
 }
+
+func convertEpochToTime(epoch: TimeInterval) -> String {
+    let date = Date(timeIntervalSince1970: epoch)
+    let formatter = DateFormatter()
+    formatter.dateFormat = "h a"
+    formatter.amSymbol = "AM"
+    formatter.pmSymbol = "PM"
+    formatter.locale = Locale.current
+    return formatter.string(from: date)
+}

e7cbb9c250ffdce777302eb005a579f4cef12128
Youssef Waleed <ywsoliman@gmail.com>
2024-05-20 20:20:41 +0300

    Created a custom back button

--- WeatherSwiftUI/TemperatureScreen/TemperatureView.swift
+++ WeatherSwiftUI/TemperatureScreen/TemperatureView.swift
@@ -9,18 +9,34 @@ import SwiftUI
 
 struct TemperatureView: View {
     
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
     var hours: [Hour]
     
     var body: some View {
         ZStack {
-            Image(.day)
+            Image(isTimeBetween5AMAnd6PM() ? .day : .night)
                 .resizable()
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .ignoresSafeArea()
             List(hours, id: \.epoch) { hour in
                 TemperatureRow(hour: hour)
             }.padding(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
-            .listStyle(.plain)
+                .listStyle(.plain)
+        }
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                customBackButton()
+            }
+        }
+    }
+    
+    func customBackButton() -> Button<Image> {
+        return Button {
+            presentationMode.wrappedValue.dismiss()
+        } label: {
+            Image(systemName: "arrowshape.turn.up.backward.fill")
         }
     }
 }
